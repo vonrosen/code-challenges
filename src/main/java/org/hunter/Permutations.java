96da37913a5099745ecd0926763849a6b3eb7e0d
hunterst <hunterst@zillowgroup.com>
2022-09-02 14:12:15 -0700

    more changes

--- src/main/java/org/hunter/Permutations.java
+++ src/main/java/org/hunter/Permutations.java
@@ -4,11 +4,44 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * https://leetcode.com/problems/permutations/
  */
 public class Permutations{
+
+	public List<List<Integer>> permute3(int [] nums){
+		return permute3(toList(nums));
+	}
+
+	private List<Integer> toList(int [] nums){
+		List<Integer> l = new ArrayList();
+		for(int i = 0; i < nums.length; ++i){
+			l.add(nums[i]);
+		}
+		return l;
+	}
+
+	public List<List<Integer>> permute3(List<Integer> nums) {
+		List<List<Integer>> lists = new ArrayList<>();
+		if(nums.size() == 1){
+			List<Integer> list = new ArrayList<>();
+			list.add(nums.get(0));
+			lists.add(list);
+			return lists;
+		}
+		for(int i = 0; i < nums.size(); ++i){
+			int value = nums.remove(0);
+			List<List<Integer>> perms = permute3(nums);
+			for(List<Integer> list : perms){
+				list.add(value);
+			}
+			lists.addAll(perms);
+			nums.add(value);
+		}
+		return lists;
+	}
 
 	public List<List<Integer>> permute(int[] nums) {
 		List<List<Integer>> lists = new ArrayList<>();
@@ -38,9 +71,9 @@ public class Permutations{
 
 	public static void main(String [] args){
 		Permutations permutations = new Permutations();
-		System.out.println(permutations.permute(new int[]{1}));
-		System.out.println(permutations.permute(new int[]{0, 1}));
-		System.out.println(permutations.permute(new int[]{1, 2, 3}));
-		System.out.println(permutations.permute(new int[]{5,4,6,2}));
+//		System.out.println(permutations.permute3(new int[]{1}));
+//		System.out.println(permutations.permute3(new int[]{0, 1}));
+		System.out.println(permutations.permute3(new int[]{1, 2, 3}));
+//		System.out.println(permutations.permute3(new int[]{5,4,6,2}));
 	}
 }
