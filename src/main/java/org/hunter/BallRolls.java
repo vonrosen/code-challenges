3c5e837d81f36c91483a477e62d15846321c5207
hunterst <hunterst@zillowgroup.com>
2022-09-30 11:17:51 -0700

    more changes

--- src/main/java/org/hunter/BallRolls.java
+++ src/main/java/org/hunter/BallRolls.java
@@ -21,11 +21,16 @@ public class BallRolls{
 		BallRolls ballRolls = new BallRolls();
 		int [] points = new int[] {3,1,0};
 		System.out.println(ballRolls.isScoreValid(4, 0, points, 0, 11));
+		System.out.println(ballRolls.isScoreValidMem(4, points, 11));
 		System.out.println(ballRolls.isScoreValid(4, 0, points, 0, 4));
+		System.out.println(ballRolls.isScoreValidMem(4, points, 4));
 		System.out.println(ballRolls.isScoreValid(1, 0, points, 0, 4));
+		System.out.println(ballRolls.isScoreValidMem(1, points,4));
 		System.out.println(ballRolls.isScoreValid(1, 0, points, 0, 1));
+		System.out.println(ballRolls.isScoreValidMem(1, points,1));
 	}
 
+	//time complexity = O(2^N) where N = points.length. space complexity = 0(1)
 	private boolean isScoreValid(int totalRolls, int rollNumber, int [] points, int currentScore,
 			int targetScore){
 		if(totalRolls == rollNumber){
@@ -37,6 +42,32 @@ public class BallRolls{
 			}
 		}
 		return false;
+	}
+
+	//DP solution
+	//time complexity = O(TR * TS * NP) where TR = total rolls, TS = target score and NP = number of points. space
+	// complexity = 0(TR * TS)
+	private boolean isScoreValidMem(int totalRolls, int [] points, int targetScore){
+		int [][] mem = new int[totalRolls][targetScore + 1];
+		for(int i = 0; i < totalRolls; ++i){
+			for(int j = 1; j < mem[0].length; ++j){
+				for(int k = 0; k < points.length; ++k){
+					if(i == 0){
+						if(points[k] < mem[0].length){
+							mem[i][points[k]] = 1;
+						}
+					}else{
+						if(mem[i - 1][j] > 0){
+							int newScore = j + points[k];
+							if(newScore < mem[0].length){
+								mem[i][newScore] = mem[i - 1][j] + 1;
+							}
+						}
+					}
+				}
+			}
+		}
+		return mem[totalRolls - 1][targetScore] > 0;
 	}
 
 }
