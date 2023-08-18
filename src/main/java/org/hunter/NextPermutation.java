0495e789c6f0adb17785da1a2b62282ae3eef221
hunterst <hunterst@zillowgroup.com>
2023-08-18 16:56:49 -0700

    more changes

--- src/main/java/org/hunter/NextPermutation.java
+++ src/main/java/org/hunter/NextPermutation.java
@@ -2,6 +2,12 @@ package org.hunter;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
