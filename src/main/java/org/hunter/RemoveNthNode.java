afd840769d7aa7ada42abc04c5fb1c104d53547c
hunterst <hunterst@zillowgroup.com>
2024-04-29 17:39:00 -0700

    Revert "more changes"
    This reverts commit 0fef0cf8a20dd55412bc9dc9f4ae4cd68ac2e3f9.

--- src/main/java/org/hunter/RemoveNthNode.java
+++ src/main/java/org/hunter/RemoveNthNode.java
@@ -53,10 +53,6 @@ class ListNode {
 	ListNode() {}
 	ListNode(int val) { this.val = val; }
 	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-
-	public String toString(){
-		return String.valueOf(val);
-	}
 }
 
 
