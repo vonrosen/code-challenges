ddf19ccf6c80678ec6e7272e0048db2f8b2c52a8
Hunter S <hunterst@zillowgroup.com>
2024-11-19 15:48:17 -0800

    Create mergeksortedlists.java

---
+++ src/main/java/org/hunter/mergeksortedlists.java
@@ -0,0 +1,44 @@
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+class mergeksortedlists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0){
+            return null;
+        }
+        return mergeKLists(lists, 0, lists.length - 1);
+	}
+
+    ListNode mergeKLists(ListNode[] lists, int start, int end){
+        if(start == end){
+            return lists[start];
+        }
+        int mid = start + (end - start) / 2;
+        ListNode left = mergeKLists(lists, start, mid);
+        ListNode right = mergeKLists(lists, mid + 1, end);
+        return mergeTwoLists(left, right);
+    }
+
+    ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null && list2 == null){
+            return null;
+        }
+        if(list1 == null){
+            return list2;
+        }
+        if(list2 == null){
+            return list1;
+        }
+        if(list1.val < list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+        list2.next = mergeTwoLists(list2.next, list1);
+        return list2;
+    }
+}
