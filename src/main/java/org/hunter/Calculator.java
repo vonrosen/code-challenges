b6ba7001483b2e18101b109c3a1baa332b6dfbd6
Hunter S <hunterst@zillowgroup.com>
2024-11-24 15:04:18 -0800

    Create Calculator.java

---
+++ src/main/java/org/hunter/Calculator.java
@@ -0,0 +1,54 @@
+package org.hunter;
+
+class Calculator {
+    public static void main(String [] args){
+        Calculator c = new Calculator();
+        System.out.println(c.calculate("4+5*10"));
+        System.out.println(c.calculate("5*10+1"));
+        System.out.println(c.calculate("5+10"));
+        System.out.println(c.calculate("5+10+3*66*33+32*3+7+7+3433"));//10,092
+    }
+
+    /*
+     * 4+5*10 = 54
+     *
+     */
+    public int calculate(String expression) {
+        int sum = 0;
+        int prod = 0;
+        StringBuilder lastNum = new StringBuilder();
+        Character lastOp = '+';
+        for(char c : expression.toCharArray()){
+            if(c == '+'){
+                int num = Integer.parseInt(lastNum.toString());
+                if(lastOp == '+'){
+                    sum += num;
+                }else{
+                    prod *= num;
+                    sum += prod;
+                    prod = 0;
+                }
+                lastOp = c;
+                lastNum = new StringBuilder();
+            }else if(c == '*'){
+                int num = Integer.parseInt(lastNum.toString());
+                if(prod == 0){
+                    prod = 1;
+                }
+                prod *= num;
+                lastOp = c;
+                lastNum = new StringBuilder();
+            }else{
+                lastNum.append(c);
+            }
+        }
+        int num = Integer.parseInt(lastNum.toString());
+        if(lastOp == '+'){
+            sum += num;
+        }else{
+            prod *= num;
+        }
+        return sum + prod;
+    }
+
+}
