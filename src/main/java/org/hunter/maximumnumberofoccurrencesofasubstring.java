8bd5fced05381bd47f9c8b69ad3ba3be4476aa3b
Hunter <hunter@Hunters-MBP-2.attlocal.net>
2024-12-02 19:21:17 -0800

    more changes

--- src/main/java/org/hunter/maximumnumberofoccurrencesofasubstring.java
+++ src/main/java/org/hunter/maximumnumberofoccurrencesofasubstring.java
@@ -7,8 +7,8 @@ class maximumnumberofoccurrencesofasubstring {
     public static void main(String [] args){
         maximumnumberofoccurrencesofasubstring s = new maximumnumberofoccurrencesofasubstring();
         System.out.println(s.maxFreq("aabcabcab", 2, 2, 3));
-
     }
+
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
         int left = 0;
         Map<Character,Integer> letters = new HashMap<>();
@@ -19,7 +19,8 @@ class maximumnumberofoccurrencesofasubstring {
             letters.putIfAbsent(c, 0);
             letters.put(c, letters.get(c) + 1);
             sb.append(c);
-            while(letters.size() > maxLetters || sb.length() > maxSize){
+            //we don't need maxSize at all!
+            while(letters.size() > maxLetters){
                 char lChar = s.charAt(left);
                 letters.put(lChar, letters.get(lChar) - 1);
                 if(letters.get(lChar) <= 0){
@@ -27,8 +28,8 @@ class maximumnumberofoccurrencesofasubstring {
                 }
                 sb.delete(0, 1);
                 left++;
-            }            
-            if(letters.size() <= maxLetters && sb.length() >= minSize && sb.length() <= maxSize){
+            }
+            if(letters.size() <= maxLetters && sb.length() == minSize){
                 answers.putIfAbsent(sb.toString(), 0);
                 answers.put(sb.toString(), answers.get(sb.toString()) + 1);
                 char lChar = s.charAt(left);
@@ -37,7 +38,7 @@ class maximumnumberofoccurrencesofasubstring {
                     letters.remove(lChar);
                 }
                 sb.delete(0, 1);
-                left++;                
+                left++;
             }
         }
         if(answers.isEmpty()){
