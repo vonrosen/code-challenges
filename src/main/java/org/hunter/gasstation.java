c337aab00980af089e1a1025ec86f82f36f94769
Hunter S <hunterst@zillowgroup.com>
2024-12-16 17:03:27 -0800

    Update gasstation.java

--- src/main/java/org/hunter/gasstation.java
+++ src/main/java/org/hunter/gasstation.java
@@ -6,32 +6,24 @@ class gasstation {
             diffs[i] = gas[index] - cost[index];
         }
         int sum = 0;
-        for(int i = 0; i < gas.length; ++i){
-            sum += diffs[i];
-        }
-        if(sum < 0){
-            return -1;
-        }
-        sum = 0;
         int left = 0;
         int right = 0;
-        while(right < diffs.length){
-            while(diffs[left] < 0 || sum < 0){
-                sum -= diffs[left];               
-                ++left;
-            }    
+        while(right < diffs.length){            
             sum += diffs[right];
+            if(sum < 0){
+                left = right + 1;
+                sum = 0;
+            }
             int length = right - left + 1;
             if(length == gas.length){
                 if(sum >= 0){
                     return left % gas.length;
                 }
                 sum = 0;
-                ++left;
+                left = right + 1;
             }
             ++right;
         }
-        //should never happen
         return -1;
     }
 }
