a2626ccfc0f0dba6719f705d644d16304f358aff
Hunter S <hunterst@zillowgroup.com>
2024-12-30 10:02:31 -0800

    Create subarrayproductlessthank.java

---
+++ src/main/java/org/hunter/subarrayproductlessthank.java
@@ -0,0 +1,21 @@
+package org.hunter;
+
+class subarrayproductlessthank {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if(k <= 1){
+            return 0;
+        }
+        int left = 0;
+        int prod = 1;
+        int ans = 0;
+        for(int right = 0; right < nums.length; ++right){
+            prod *= nums[right];
+            while(prod >= k){
+                prod /= nums[left];
+                ++left;
+            }
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+}
