65230ce9255662a89c847148bb610c02f4762649
Hunter Stern <hunters@meta.com>
2026-02-03 07:57:08 -0800

    more changes

---
+++ search_rotated_array.py
@@ -0,0 +1,52 @@
+class Solution:
+    #O(LogN)
+    def search(self, nums: list[int], target: int) -> int:
+
+        def find_pivot(nums):
+            left = 0
+            right = len(nums) - 1
+            while left <= right:
+                mid = int(left + (right - left) / 2)
+                mid_value = nums[mid]
+
+                if mid > 0 and mid < len(nums) - 1 and nums[mid - 1] > mid_value < nums[mid + 1]:
+                    return mid
+
+                if mid_value > nums[right]:
+                    left = mid + 1
+                elif mid_value < nums[left]:
+                    right = mid - 1
+                else:
+                    right = mid - 1
+            return left
+
+        def bin_search(nums, target, start, end):
+            while start <= end:
+                mid = int(start + (end - start) / 2)
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] < target:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            return -1
+
+        pivot = find_pivot(nums)
+        if pivot == 0:
+            return bin_search(nums,target, 0, len(nums) - 1)
+        else:
+            s1 = bin_search(nums, target, 0, pivot - 1)
+            s2 = bin_search(nums, target, pivot, len(nums) - 1)
+            return max(s1, s2)
+
+
+sol = Solution()
+print(sol.search([4,5,6,7,0,1,2], 0))
+print(sol.search([4,5,6,7,0,1,2], 3))
+print(sol.search([1], 0))
+print(sol.search([5,1,3], 5))
+print(sol.search([3,1], 0))
+
+# print(sol.search([1,2,0], 0))
+# print(sol.search([0,1,2,3], 0))
+
